408b99774a7783fbcdb1c74ce07d8f699df2b831
rfernandez135 <friendlyfern@gmail.com>
2025-03-31 16:23:13 -0700

    1) Removed TemplateFiles folder from .gitignore. 2) Added two template files that a user can load.

---
+++ TemplateFiles/fileForPublishing.m
@@ -0,0 +1,54 @@
+%%
+% <html>
+% <body>
+% <h1> File Information </h1> 
+% <ul>
+% <li> <p><b>Author:</b> </p>
+% <li> <p><b>Date:</b> </p>
+% <li> <p><b>File Name:</b> </p>
+% <li> <p><b>Purpose:</b> </p>
+% </ul>
+% <h1>Notes from the Author</h1>
+% <ol>
+% <li> <p></p>
+% <li> <p></p>
+% <li> <p></p>
+% <li> <p></p>
+% </ol>
+% </body> 
+% </html>
+%
+%% Begin Logic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TemplateFiles/functionHeader.m
@@ -0,0 +1,33 @@
+%%
+%
+% Author: 
+% Date Created:
+% File Name:
+% Developed in MATLAB version:
+% Purpose:
+% 
+%% Inputs
+%
+% #
+%
+%
+%% Outputs
+% #
+%
+%
+%% Notes
+% #
+%
+%
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+
+
+%% Begin function
+function
+
+
+
+end %end function.m
+
+
+
